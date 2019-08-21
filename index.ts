af14c26a6faccd52e536ecfeed7e51c7afef2fd5
imed jaberi <imed_jebari@hotmail.fr>
2019-08-21 03:42:02 +0200

    add main code 🛰 ..

--- index.ts
+++ index.ts
@@ -1,1 +1,21 @@
-// init // 
+import PasswordCrypt from './src/main';
+
+/**
+ * crypto the password .. 
+ * @param Secret : private secret key for customize the crypto .. 
+ * @param Password : the passwrod value .. 
+ */
+export async function Crypt(Secret:string|number , Password:string ):Promise<string>{
+  return await new PasswordCrypt(Secret,Password).pcrypt();
+};
+
+ /**
+  * compare the old crypto pasword with the new for check .... 
+  * @param Secret : private secret key for customize the crypto .. 
+  * @param Password : the passwrod value .. 
+  * @param Hash : the result of crypto ancien password ..  
+  */
+export async function Compare(Secret:string|number , Password:string , Hash:string ):Promise<boolean>{
+  return await new PasswordCrypt(Secret,Password,Hash).compare_pcrypt();
+};
+
