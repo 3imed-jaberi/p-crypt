be1594035cb46f7e8dcf83e54079aeb506585f87
3imed-jaberi <imed_jebari@hotmail.fr>
2020-04-24 05:34:44 +0200

    improve test --old --fine --too --mocha ☕️ --chai 🍵 ..

--- test.spec.ts
+++ test.spec.ts
@@ -3,16 +3,18 @@ import { expect } from 'chai';
 import { Compare, Crypt } from '.';
 
 
-
 describe('unit test using ` mocha ☕️ ` & ` chai 🍵 ` is running 👻 ..', () => {
 
-  let Password: string = 'Imed Jaberi', Secret: string|number = 'SomeSecret', Hash: string = '$2a$10$58t0eKJocSAok2bIAcztSe3pU7fuuF9P72c1ZIUsIX84WCKEuZPIi';
+  let Password: string = 'Imed Jaberi', Secret: string|number = 'SomeSecret';
 
-  it('Check the new password with the old ..', async () => {
-    expect(await Compare(Secret, Password, Hash)).to.be.true;
+  it('Compare Test ..', async () => {
+    let HashedPassword = await Crypt(Secret, Password);
+    expect(await Compare(Secret, Password, HashedPassword)).to.be.true;
   });
 
-  it('Generate the hash code ..', async () => {
-    expect(await Crypt(Secret, Password)).to.not.equal(Password);
+  it('Crypt Test ..', async () => {
+    (expect(await Crypt(Secret, Password)).to.not.equal(await Crypt(Secret, Password)) &&
+    expect(await Crypt(Secret, Password)).to.not.equal(Password));
   });
+
 });
