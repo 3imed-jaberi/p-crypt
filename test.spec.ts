8dd5c558d0ba8b0cb49cca67a73a897a9883a0c9
3imed-jaberi <imed_jebari@hotmail.fr>
2020-11-13 22:48:41 +0100

    coverage 100% --💯 --mocha ☕️ --chai 🍵 ..

--- test.spec.ts
+++ test.spec.ts
@@ -1,20 +1,23 @@
-import 'mocha';
-import { expect } from 'chai';
-import { Compare, Crypt } from '.';
+import 'mocha'
+import { expect } from 'chai'
+import { Compare, Crypt } from '.'
 
 
 describe('unit test using ` mocha ☕️ ` & ` chai 🍵 ` is running 👻 ..', () => {
-
-  let Password: string = 'Imed Jaberi', Secret: string|number = 'SomeSecret';
+  const password = 'Imed Jaberi'
+  const secret = 'SomeSecret'
 
   it('Compare Test ..', async () => {
-    let HashedPassword = await Crypt(Secret, Password);
-    expect(await Compare(Secret, Password, HashedPassword)).to.be.true;
-  });
+    expect(await Compare(
+      secret,
+      password,
+      await Crypt(secret, password)
+    )).to.be.true
+  })
 
   it('Crypt Test ..', async () => {
-    (expect(await Crypt(Secret, Password)).to.not.equal(await Crypt(Secret, Password)) &&
-    expect(await Crypt(Secret, Password)).to.not.equal(Password));
-  });
-
-});
+    expect(await Crypt(secret,password))
+    .to.not.equal(await Crypt(secret, password)) &&
+    expect(await Crypt(secret, password)).to.not.equal(password)
+  })
+})
